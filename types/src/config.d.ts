d11848970c379269edf6267369f538a0e3684345
trapcodeio <trapcode.io@gmail.com>
2020-12-05 20:10:14 +0100

    Updated Types for typescript

--- types/src/config.d.ts
+++ types/src/config.d.ts
@@ -1,3 +1,3 @@
-import { DollarSign } from "xpresser/types";
+import type { DollarSign } from "xpresser/types";
 export declare const $: DollarSign;
 export declare const pluginConfig: import("object-collection");
