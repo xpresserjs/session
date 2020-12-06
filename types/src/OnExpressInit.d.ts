0bcbd94f472ba84b1dfab0fdb9c2eaa8b8cf3cf6
trapcodeio <trapcode.io@gmail.com>
2020-12-06 10:44:08 +0100

    Removed import of dec files

--- types/src/OnExpressInit.d.ts
+++ types/src/OnExpressInit.d.ts
@@ -1,2 +1,3 @@
-declare const _default: (next: () => void) => void;
+import type { DollarSign } from "xpresser/types";
+declare const _default: (next: () => void, $: DollarSign) => void;
 export = _default;
--- types/src/index.d.ts
+++ types/src/index.d.ts
@@ -1,2 +1,2 @@
-import "../xpresser";
-export declare function run(): void;
+import type { DollarSign } from "xpresser/types";
+export declare function run(config: any, $: DollarSign): void;
